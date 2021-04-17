70a1f71cc9c985d503af9463ca1ecf70db461a9f
mohammed sarfaraz <sarfarazmohammed187@gmail.com>
2021-04-17 23:57:20 +0530

    Update event in serverless completed

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
   app.initialized().then(function(_client) {
-    var client = _client;
+    window.client = _client;
     client.events.on('app.activated', function() {
       console.clear();
       console.log("!!! App Activated !!!");
@@ -10,10 +10,63 @@ $(document).ready( function() {
 });
 
 var eventCallback = function (event) {
-  console.log(event.type + " event occurred");
   var event_data = event.helper.getData();
-  console.log({event_data});
-  
-  event.helper.done()
-  // event.helper.fail('error')
+  event.helper.done();
+  if(event_data.changedAttributes.isCustomFieldsChanged[1] === true) {
+    getTicketDetails();
+  } else {
+    console.log(false);
+  }
 };
+
+function getTicketDetails () {
+  Promise.all([client.data.get("ticket"), client.data.get("contact")]).then(function([data, contact]) {
+    console.log(contact);
+    if(data.ticket.type == "Service Task") {
+      const entity = client.db.entity({ version: 'v1' });
+      const fsmRecords = entity.get('fsm_records');
+      fsmRecords.getAll({
+        query : { 'ticket_id': data.ticket.id }
+      }).then(function(dbData) {
+        console.log("Records from DB :", dbData.records);
+        dbData.records.length > 0 ? checkFieldsUpdate(dbData.records[0], data.ticket, contact) : '';
+      }).catch(err => console.log(err));
+    }
+  }, function(error) {
+    console.log(error);
+  });
+}
+
+function checkFieldsUpdate(record, ticketDetails, contactDetails) {
+  console.log("Before : ", record.data);
+  var fieldNames = Object.keys(record.data).filter(function(e) { 
+    return this.indexOf(e) < 0
+  }, ['responder_id', 'ticket_id']);
+  let flag = false;
+  fieldNames.forEach(element => {
+    if(ticketDetails.custom_fields[element] !== record.data[element]) {  
+      flag = true
+      if(element == 'cf_fsm_phone_number') {
+        record.data[element] = Number(ticketDetails.custom_fields[element]);
+      } else if((element === 'cf_fsm_appointment_start_time' && 'cf_fsm_appointment_end_time')) {
+        record.data[element] = new Date(ticketDetails.custom_fields[element]).toISOString()
+      } else {
+        record.data[element] = ticketDetails.custom_fields[element]
+      }
+    } 
+  });
+  console.log("After : ", record.data, flag);
+  updateCalendarEvent(flag, record.data);
+}
+
+function updateCalendarEvent(flag, record) {
+  if(flag) {
+    console.log("!!!!!! Test !!!!!!");
+    var options = { "url" : "test" };
+    client.request.invoke("serverMethod", options).then(function(data) {
+      console.log(data);
+    }, function(err) {
+      console.log(err);
+    });
+  }
+}
--- server/server.js
+++ server/server.js
@@ -7,78 +7,115 @@ exports = {
   ],
   onTicketCreateCallback: async function(payload) {
     console.log("----------------- On Ticket Create ---------------------");
-    checkTicketType(payload);
+    checkTicketType(payload, '');
   },
   onTicketUpdateCallback: async function(payload) {
-    console.log("----------------- On Ticket Update ---------------------");
-    let ticketDetails = payload.data.ticket;
+    console.log(`----------------- On Ticket Update : ${payload.data.ticket.id} ---------------------`);
     const $entity = $db.entity({ version: 'v1' });
     const responseFromDb = await $entity.get('fsm_records');
-    console.log(ticketDetails.id)
     responseFromDb.getAll({
-      query : { 'ticket_id': ticketDetails.id }
-    }).then(function(data) {
-      console.log("Data : ", data);
-    }).catch(function(err) {
+      query : { 'ticket_id': payload.data.ticket.id }
+    }).then(function(dbData) {
+      console.log(dbData.records);
+      (dbData.records.length === 0) ? checkTicketType(payload, '') : checkTicketType(payload, dbData.records[0]) ;
+    }, err => {
       console.log(err);
     });
+  },
+  serverMethod : async function (payload) {
+    console.log("----------------- SMI Update Event ---------------------");
+    console.log(payload);
+    renderData(null, { "key": "value" });
   }
 }
 
-async function checkTicketType(payload) {
+async function checkTicketType(payload, record) {
   let ticketDetails = payload.data.ticket;
-  // console.log(ticketDetails);
   let start_time = ticketDetails.custom_fields["cf_fsm_appointment_start_time"];
-  let end_time = ticketDetails.custom_fields["cf_fsm_appointment_end_time"]
+  let end_time = ticketDetails.custom_fields["cf_fsm_appointment_end_time"];
   if((ticketDetails.type === "Service Task") && (ticketDetails.responder_id !== null) && start_time && end_time) {
     let response = await helpers.requestApi({ url: `api/v2/agents/${ticketDetails.responder_id}`, method: 'get', iparams : payload.iparams });
-    // console.log(JSON.parse(response));
     let agentDetails = JSON.parse(response);
-    createCalendarEvent(ticketDetails, agentDetails, start_time, end_time, payload.iparams);
+    record.data.event_id ? updateCalendarEvent(ticketDetails, agentDetails, payload.iparams, record) : createCalendarEvent(ticketDetails, agentDetails, payload.iparams);
   }
 }
 
-async function createCalendarEvent(ticketDetails, agentDetails, start_time, end_time, iparams) {
-  let descriptionBody = `    
-    <b>Contact Name : </b>${ticketDetails.custom_fields["cf_fsm_contact_name"]}
-    <b>Phone Number : </b>${ticketDetails.custom_fields["cf_fsm_phone_number"]}
-    <b>Description : </b>${ticketDetails.description_text}`
+async function createCalendarEvent(ticketDetails, agentDetails, iparams) {
   if(agentDetails.contact.email) {
-    let eventPayload = {
-      'summary': ticketDetails.subject,
-      'location': ticketDetails.custom_fields["cf_fsm_service_location"],
-      'description': descriptionBody ,
-      'start': {
-        'dateTime': start_time,
-        'timeZone': 'UTC',
-      },
-      'end': {
-        'dateTime': end_time,
-        'timeZone': 'UTC',
-      },
-      'attendees': [
-        {'email': agentDetails.contact.email }
-      ],
-    }
+    let eventPayload = await getEventObj(ticketDetails, agentDetails);
     console.log("Event payload on Create : ", eventPayload);
     let response = await helpers.calendarApi({ method : 'post', type : `calendars/${iparams.calendarId}/events`, body : JSON.stringify(eventPayload), iparams : iparams });
     console.log(response);
-    (response.status == 200 || 202) ? storeInDb(ticketDetails, agent_email, start_time, end_time) : '';
-    // storeInDb(ticketDetails, agentDetails, start_time, end_time, iparams);
+    let eventID = JSON.parse(response.response).id;
+    console.log(eventID);    
+    (response.status == 200 || 202) ? storeInDB(ticketDetails, agentDetails, eventID) : '';
+    // storeInDB(ticketDetails, agentDetails, iparams);
   }
 }
 
-async function storeInDb (ticketDetails, agentDetails, start_time, end_time) {
+async function updateCalendarEvent(ticketDetails, agentDetails, iparams, record) {
+  console.log("------------- Inside Update Event -------------");
+  if(agentDetails.contact.email) {
+    let eventPayload = await getEventObj(ticketDetails, agentDetails);
+    let response = await helpers.calendarApi({ method : 'put', type : `calendars/${iparams.calendarId}/events/${record.data.event_id}`, body : JSON.stringify(eventPayload), iparams : iparams });
+    let eventID = JSON.parse(response.response).id;
+    console.log("Event ID on update :", eventID);
+    (response.status == 200 || 202) ? updateInDB(ticketDetails, agentDetails, record.display_id, eventID) : '';
+  }
+}
+
+async function storeInDB(ticketDetails, agentDetails, eventID) {
   const $entity = $db.entity({ version: 'v1' });
-  const responseFromDb = await $entity.get('fsm_records').create({
+  let dbObj = await getDbeObj(ticketDetails, agentDetails, eventID);
+  const responseFromDb = await $entity.get('fsm_records').create(dbObj);
+  // .then(data => console.log(data)).catch(err => console.log(err));
+  console.log(responseFromDb);
+}
+
+async function updateInDB(ticketDetails, agentDetails, display_id, eventID) {
+  const $entity = $db.entity({ version: 'v1' });
+  let dbObj = await getDbeObj(ticketDetails, agentDetails, eventID); 
+  const responseFromDb = await $entity.get('fsm_records')
+  responseFromDb.update(display_id, dbObj).then(dbData => {
+    console.log("Response from Update : ", dbData);
+  }, err => {
+    console.log(err);
+  });
+}
+
+async function getEventObj(ticketDetails, agentDetails) {
+  let descriptionBody = `    
+    <b>Contact Name : </b>${ticketDetails.custom_fields["cf_fsm_contact_name"]}
+    <b>Phone Number : </b>${ticketDetails.custom_fields["cf_fsm_phone_number"]}
+    <b>Description : </b>${ticketDetails.description_text}`;
+  let obj = {
+    'summary': ticketDetails.subject,
+    'location': ticketDetails.custom_fields["cf_fsm_service_location"],
+    'description': descriptionBody ,
+    'start': {
+      'dateTime': ticketDetails.custom_fields["cf_fsm_appointment_start_time"],
+      'timeZone': 'UTC',
+    },
+    'end': {
+      'dateTime': ticketDetails.custom_fields["cf_fsm_appointment_end_time"],
+      'timeZone': 'UTC',
+    },
+    'attendees': [
+      {'email': agentDetails.contact.email }
+    ]
+  }
+  return obj
+}
+
+async function getDbeObj(ticketDetails, agentDetails, eventID) {
+  return {
     'ticket_id': ticketDetails.id,
+    'event_id' : eventID,
     'responder_id' : agentDetails.id,
     'cf_fsm_contact_name': ticketDetails.custom_fields["cf_fsm_contact_name"],
     'cf_fsm_service_location' : ticketDetails.custom_fields["cf_fsm_service_location"],
     'cf_fsm_phone_number' : Number(ticketDetails.custom_fields["cf_fsm_phone_number"]),
-    'cf_fsm_appointment_start_time' : new Date(start_time).toISOString(),
-    'cf_fsm_appointment_end_time' : new Date(end_time).toISOString()
-  });
-  // .then(data => console.log(data)).catch(err => console.log(err));
-  console.log({ responseFromDb })
+    'cf_fsm_appointment_start_time' : new Date(ticketDetails.custom_fields["cf_fsm_appointment_start_time"]).toISOString(),
+    'cf_fsm_appointment_end_time' : new Date(ticketDetails.custom_fields["cf_fsm_appointment_end_time"]).toISOString()
+  }
 }
